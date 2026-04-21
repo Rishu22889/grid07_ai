162e46cfde38370e9771f3f5ff11a1f9a51e7dda
Rishu <rk1258106@gmail.com>
2026-04-22 00:14:10 +0530

    added embedding_model

--- app/embeddings/embedding_model.py
+++ app/embeddings/embedding_model.py
@@ -1,8 +1,31 @@
 # Embedding model setup
 # app/embeddings/embedding_model.py
 
-import os
 from __future__ import annotations
+from functools import lru_cache
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from google.generativeai import genai
+from app.config.settings import EMBEDDING_MODEL, GOOGLE_API_KEY
+
+@lru_cache(maxsize=1)
+def get_embedding_model() -> GoogleGenerativeAIEmbeddings:
+    if not GOOGLE_API_KEY:
+        raise EnvironmentError("GOOGLE_API_KEY is not set in environment variables.")
+    
+    print(f"[Embeddings] Loading model: {EMBEDDING_MODEL}")
+
+    embedding_model = GoogleGenerativeAIEmbeddings(model=EMBEDDING_MODEL, api_key=GOOGLE_API_KEY)
+
+    return embedding_model
+
+
+def embed_text(text: list[str]) -> list[float]:
+    model = get_embedding_model()
+    embedding = model.embed_query(text)
+    return embedding
+
+def embed_doc(text: list[str]) -> list[float]:
+    model = get_embedding_model()
+    embedding = model.embed_documents(text)
+    return embedding
+
 
